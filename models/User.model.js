aa7c7eb04f4dda3f0ec21a2e5ce8a321430709a8
bulbulahmed9 <bulbulahmed.tk@gmail.com>
2019-10-28 18:51:27 -0700

    product controller done

--- models/User.model.js
+++ models/User.model.js
@@ -26,7 +26,8 @@ const UserSchema = new mongoose.Schema({
       default: false
   },
   stripeSeller: {},
-  stripeCustomer: {}
+  stripeCustomer: {},
+  order: []
 });
 
 module.exports = User = mongoose.model("User", UserSchema);
