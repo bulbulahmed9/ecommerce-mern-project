8254e147837ccd51fa29896e40ba5e793dcf4938
bulbulahmed9 <bulbulahmed.tk@gmail.com>
2019-11-23 19:26:02 -0800

    server file modified

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
-
+const path = require('path');
 const app = express();
 
 // Connect DB
@@ -16,9 +16,15 @@ app.use("/api/products", require("./routes/api/product.routes"));
 app.use("/api/cart", require("./routes/api/cart.routes")); 
 app.use("/api/order", require("./routes/api/order.routes"));  
 
-app.get("/", (req, res) => {
-  res.send("Running");
-});
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
